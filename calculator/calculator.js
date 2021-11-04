63138cd1522f2846af70e19db94451418698ee7a
deepthi <deepthimukundan27@gmail.com>
2021-11-04 21:54:24 +0530

    Simple calculator

---
+++ calculator/calculator.js
@@ -0,0 +1,14 @@
+function disp(op){
+    document.getElementById("result").value+=op;
+}
+function solve() {
+    let y=eval(document.getElementById("result").value);
+    document.getElementById("result").value=y;
+}
+function clear() {
+    document.getElementById("result").value="";
+}
+function backSpace() {
+    let val=document.getElementById("result").value;
+    document.getElementById("result").value=val.substr(0,val.length-1);
+}
